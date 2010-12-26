126a5afc4ea1efb06b044c393bf806722c760def
chengdh <cheng.donghui@gmail.com>
2010-12-26 15:36:21 +0800

    基本完成了网站

---
+++ public/javascripts/mylibs/jquery.xdomainajax.js
@@ -0,0 +1,52 @@
+/**
+ * jQuery.ajax mid - CROSS DOMAIN AJAX 
+ * ---
+ * @author James Padolsey (http://james.padolsey.com)
+ * @version 0.11
+ * @updated 12-JAN-10
+ * ---
+ * Note: Read the README!
+ * ---
+ * @info http://james.padolsey.com/javascript/cross-domain-requests-with-jquery/
+ */
+
+jQuery.ajax = (function(_ajax) {
+
+	var protocol = location.protocol,
+	hostname = location.hostname,
+	exRegex = RegExp(protocol + '//' + hostname),
+	YQL = 'http' + (/^https/.test(protocol) ? 's': '') + '://query.yahooapis.com/v1/public/yql',
+	query = 'select * from html where url="{URL}" and xpath="*"';
+
+	function isExternal(url) {
+		return ! exRegex.test(url) && /:\/\//.test(url);
+	}
+
+	return function(o) {
+		o = $.extend({
+			type: 'get',
+			dataType: 'html'
+		},
+		o);
+
+		var url = o.url;
+
+		if (/get/i.test(o.type) && isExternal(url)) {
+			//测试dataType
+			if (/json/i.test(o.dataType)) query = 'select * from json where url="{URL}"';
+			if (/xml/i.test(o.dataType)) query = 'select * from xml where url="{URL}"';
+
+			// Manipulate options so that JSONP-x request is made to YQL
+			o.url = YQL;
+
+			o.data = {
+				q: query.replace('{URL}', url + (o.data ? (/\?/.test(url) ? '&': '?') + jQuery.param(o.data) : '')),
+				format: 'xml'
+			};
+
+		}
+		return _ajax.apply(this, arguments);
+
+	};
+})(jQuery.ajax);
+
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -18,9 +18,48 @@ $(document).ready(function() {
 		thumb: false
 
 	});
+	$('#breadcrumb_bar').jBreadCrumb();
+	//fancybox
+	$('.fancybox').fancybox();
+	//查找运单
+	$('#webysd').watermark('请录入运单编号查询');
+	$('#btn_search').live('click', function() {
+		$.fancybox.close();
+		if ($('#webysd').val() == "") {
+			$.notifyBar({
+				html: "请录入运单号查询运单信息!",
+				delay: 2000,
+				animationSpeed: "normal",
+				cls: 'error'
+			});
+
+			return;
+		}
+		$.ajax({
+			url: "http://www.yanzhaowuliu.com:8888/lmis/servlet/SelectWebServlet?ztra=hwcx&webysd=" + $('#webysd').val(),
+			type: 'get',
+			dataType: 'xml',
+			success: function(xml) {
+				var ret_result = $(xml).find('town').first();
+				var alert_html = "";
+				alert_html += "<p class='text-center'>" + ret_result.attr('StrYsd') + "</p>";
+				alert_html += "<p>" + ret_result.attr('hw') + "</p>";
+				alert_html += "<p class='text-center'>" + ret_result.attr('fhr') + "</p>";
+				alert_html += "<p class='text-center'>" + ret_result.attr('hk') + "</p>";
+				alert_html += "<p class='text-center'>" + ret_result.attr('tkzt') + "</p>";
+				$.notifyBar({
+					html: alert_html,
+					delay: 5000,
+					animationSpeed: "normal",
+					cls: 'success'
+				});
+			}
+
+		});
+	});
 	//初始化google地图
-	$('#service_net_map').jMapping({default_zoom_level : 10});
-        $('#breadcrumb_bar').jBreadCrumb();
+	$('#service_net_map').jMapping();
+
 
 });
 

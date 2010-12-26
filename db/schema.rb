c1a320ea23dee2c995790f81c765dc9b60280499
chengdh <cheng.donghui@gmail.com>
2010-12-26 19:32:42 +0800

    修正了乱码问题

--- db/schema.rb
+++ db/schema.rb
@@ -12,99 +12,93 @@
 ActiveRecord::Schema.define(:version => 20091003095744) do
 
   create_table "assets", :force => true do |t|
-    t.column "caption", :string
-    t.column "title", :string
-    t.column "asset_file_name", :string
-    t.column "asset_content_type", :string
-    t.column "asset_file_size", :integer
-    t.column "created_by_id", :integer
-    t.column "updated_by_id", :integer
-    t.column "created_at", :datetime
-    t.column "updated_at", :datetime
+    t.string   "caption"
+    t.string   "title"
+    t.string   "asset_file_name"
+    t.string   "asset_content_type"
+    t.integer  "asset_file_size"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", :force => true do |t|
+    t.integer  "page_id"
+    t.string   "author"
+    t.string   "author_url"
+    t.string   "author_email"
+    t.string   "author_ip"
+    t.text     "content"
+    t.text     "content_html"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "filter_id",    :limit => 25
+    t.string   "user_agent"
+    t.string   "referrer"
+    t.datetime "approved_at"
+    t.integer  "approved_by"
+    t.string   "mollom_id"
   end
 
   create_table "config", :force => true do |t|
-    t.column "key", :string, :limit => 40, :default => "", :null => false
-    t.column "value", :string, :default => ""
+    t.string "key",   :limit => 40, :default => "", :null => false
+    t.string "value",               :default => ""
   end
 
   add_index "config", ["key"], :name => "key", :unique => true
 
   create_table "extension_meta", :force => true do |t|
-    t.column "name", :string
-    t.column "schema_version", :integer, :default => 0
-    t.column "enabled", :boolean, :default => true
-  end
-
-  create_table "google_maps", :force => true do |t|
-    t.column "name", :string, :null => false
-    t.column "description", :string
-    t.column "center", :point, :null => false
-    t.column "zoom", :integer
-    t.column "created_at", :datetime
-    t.column "updated_at", :datetime
-    t.column "created_by_id", :integer
-    t.column "updated_by_id", :integer
-    t.column "style", :integer
+    t.string  "name"
+    t.integer "schema_version", :default => 0
+    t.boolean "enabled",        :default => true
   end
 
   create_table "layouts", :force => true do |t|
-    t.column "name", :string, :limit => 100
-    t.column "content", :text
-    t.column "created_at", :datetime
-    t.column "updated_at", :datetime
-    t.column "created_by_id", :integer
-    t.column "updated_by_id", :integer
-    t.column "content_type", :string, :limit => 40
-    t.column "lock_version", :integer, :default => 0
-  end
-
-  create_table "markers", :force => true do |t|
-    t.column "google_map_id", :integer, :null => false
-    t.column "name", :string, :null => false
-    t.column "title", :string, :null => false
-    t.column "content", :text
-    t.column "position", :point, :null => false
-    t.column "filter_id", :string, :limit => 25
-    t.column "created_at", :datetime
-    t.column "updated_at", :datetime
-    t.column "created_by_id", :integer
-    t.column "updated_by_id", :integer
-    t.column "zoom", :integer
+    t.string   "name",          :limit => 100
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.string   "content_type",  :limit => 40
+    t.integer  "lock_version",                 :default => 0
   end
 
   create_table "page_attachments", :force => true do |t|
-    t.column "asset_id", :integer
-    t.column "page_id", :integer
-    t.column "position", :integer
+    t.integer "asset_id"
+    t.integer "page_id"
+    t.integer "position"
   end
 
   create_table "page_parts", :force => true do |t|
-    t.column "name", :string, :limit => 100
-    t.column "filter_id", :string, :limit => 25
-    t.column "content", :text
-    t.column "page_id", :integer
+    t.string  "name",      :limit => 100
+    t.string  "filter_id", :limit => 25
+    t.text    "content"
+    t.integer "page_id"
   end
 
   add_index "page_parts", ["page_id", "name"], :name => "parts_by_page"
 
   create_table "pages", :force => true do |t|
-    t.column "title", :string
-    t.column "slug", :string, :limit => 100
-    t.column "breadcrumb", :string, :limit => 160
-    t.column "class_name", :string, :limit => 25
-    t.column "status_id", :integer, :default => 1, :null => false
-    t.column "parent_id", :integer
-    t.column "layout_id", :integer
-    t.column "created_at", :datetime
-    t.column "updated_at", :datetime
-    t.column "published_at", :datetime
-    t.column "created_by_id", :integer
-    t.column "updated_by_id", :integer
-    t.column "virtual", :boolean, :default => false, :null => false
-    t.column "lock_version", :integer, :default => 0
-    t.column "description", :string
-    t.column "keywords", :string
+    t.string   "title"
+    t.string   "slug",            :limit => 100
+    t.string   "breadcrumb",      :limit => 160
+    t.string   "class_name",      :limit => 25
+    t.integer  "status_id",                      :default => 1,     :null => false
+    t.integer  "parent_id"
+    t.integer  "layout_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "published_at"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.boolean  "virtual",                        :default => false, :null => false
+    t.integer  "lock_version",                   :default => 0
+    t.string   "description"
+    t.string   "keywords"
+    t.boolean  "enable_comments",                :default => false
+    t.integer  "comments_count",                 :default => 0
   end
 
   add_index "pages", ["class_name"], :name => "pages_class_name"
@@ -113,43 +107,43 @@ ActiveRecord::Schema.define(:version => 20091003095744) do
   add_index "pages", ["virtual", "status_id"], :name => "pages_published"
 
   create_table "sessions", :force => true do |t|
-    t.column "session_id", :string
-    t.column "data", :text
-    t.column "updated_at", :datetime
+    t.string   "session_id"
+    t.text     "data"
+    t.datetime "updated_at"
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "snippets", :force => true do |t|
-    t.column "name", :string, :limit => 100, :default => "", :null => false
-    t.column "filter_id", :string, :limit => 25
-    t.column "content", :text
-    t.column "created_at", :datetime
-    t.column "updated_at", :datetime
-    t.column "created_by_id", :integer
-    t.column "updated_by_id", :integer
-    t.column "lock_version", :integer, :default => 0
+    t.string   "name",          :limit => 100, :default => "", :null => false
+    t.string   "filter_id",     :limit => 25
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.integer  "lock_version",                 :default => 0
   end
 
   add_index "snippets", ["name"], :name => "name", :unique => true
 
   create_table "users", :force => true do |t|
-    t.column "name", :string, :limit => 100
-    t.column "email", :string
-    t.column "login", :string, :limit => 40, :default => "", :null => false
-    t.column "password", :string, :limit => 40
-    t.column "created_at", :datetime
-    t.column "updated_at", :datetime
-    t.column "created_by_id", :integer
-    t.column "updated_by_id", :integer
-    t.column "admin", :boolean, :default => false, :null => false
-    t.column "designer", :boolean, :default => false, :null => false
-    t.column "notes", :text
-    t.column "lock_version", :integer, :default => 0
-    t.column "salt", :string
-    t.column "session_token", :string
-    t.column "locale", :string
+    t.string   "name",          :limit => 100
+    t.string   "email"
+    t.string   "login",         :limit => 40,  :default => "",    :null => false
+    t.string   "password",      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "created_by_id"
+    t.integer  "updated_by_id"
+    t.boolean  "admin",                        :default => false, :null => false
+    t.boolean  "designer",                     :default => false, :null => false
+    t.text     "notes"
+    t.integer  "lock_version",                 :default => 0
+    t.string   "salt"
+    t.string   "session_token"
+    t.string   "locale"
   end
 
   add_index "users", ["login"], :name => "login", :unique => true

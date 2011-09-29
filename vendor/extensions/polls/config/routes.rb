5087658391cc75b11a82f1948a2b8df9dfe3d6ec
chengdh <cheng.donghui@gmail.com>
2011-09-29 10:58:23 +0800

    reinstall polls

--- vendor/extensions/polls/config/routes.rb
+++
@@ -1,6 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.namespace :admin, :member => { :clear_responses => :post } do |admin|
-    admin.resources :polls
-  end
-  map.resources :poll_response, :only => [ :index, :create ], :path_prefix => '/pages/:page_id', :controller => 'poll_response'
-end
--- vendor/extensions/polls/db/migrate/003_add_start_date_to_polls.rb
+++
@@ -1,9 +0,0 @@
-class AddStartDateToPolls < ActiveRecord::Migration
-  def self.up
-    add_column :polls, :start_date, :date
-  end
-
-  def self.down
-    remove_column :polls, :start_date
-  end
-end
